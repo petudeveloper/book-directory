c8f73d5281c38bad9f496d6c9120214196af0623
petudeveloper <davidalvarezmazzo@gmail.com>
2021-11-17 22:19:12 -0500

    create server using express and and REST endpoints

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+let books = [
+  {
+    "id": 1,
+    "title": "Clean Code",
+    "author": "Robert C. Martin"
+  }
+]
+
+app.get('/', (req, resp) => {
+  resp.send("<div><h1>Welcome to Book directory</h1><p>API endpoints</p><ul><li>'/api/books': get all the books</li></ul></div>")
+})
+
+app.get('/api/books', (req, resp) => {
+  resp.json(books)
+})
+
+app.get('/api/books/:id', (req, resp) => {
+  const id = parseInt(req.params.id)
+  const book = books.find((book) => book.id === id)
+  if (book) {
+    resp.json(book)
+  } else {
+    resp.send(404).end()
+  }
+})
+
+app.delete('/api/books/:id', (req, resp) => {
+  const id = parseInt(req.params.id)
+  const book = books.find((book) => book.id === id)
+  books = books.filter((book) => book.id != id)
+  resp.json(book)
+})
+
+app.post('/api/books/', (req, resp) => {
+  const body = req.body
+  const id = books.length + 1
+  const newBook = {
+    "id": id,
+    "title": body.title,
+    "author": body.author
+  }
+  books = [...books, newBook]
+  if (!body || !body.title || !body.author){
+    return resp.status(404).json({
+      error: "book info is missing in request body"
+    })
+  } else {
+    resp.json(newBook)
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+})
